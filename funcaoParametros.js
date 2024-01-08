4b3f881e4f5e839c3c3d27fc053846a3553c1714
MarlyssonProg <designprogthe@gmail.com>
2024-01-07 23:39:35 -0300

    função com comentarios explicativos

---
+++ funcaoParametros.js
@@ -0,0 +1,13 @@
+// Declaração de função com (parâmetro/variável) TIPO 
+
+function torrarPao(tipo) {
+    console.log('Torrando pão, ' + tipo)
+}
+
+// Chamado da funcao torrarPao, atribuindo o 'árabe' a variável tipo 
+// criada na declaração da função torrarPao  
+torrarPao('árabe')
+
+// Obs: Toda variável declarada dentro do escopo da função não poderá ser
+//      de forma global fora da função, está só existirá dentro da função
+//      a qual foi declarada
