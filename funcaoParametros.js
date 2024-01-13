d2eaaa66ed6b893f847243c0a70cecbc8071a544
MarlyssonProg <designprogthe@gmail.com>
2024-01-13 17:24:56 -0300

    Estudo de caso para definição de um valor padrão atribuido dentro da criação da função, e visível quando não se passar valor em seu chamado

--- funcaoParametros.js
+++ funcaoParametros.js
@@ -2,14 +2,19 @@
    Como o parâmetro tipo, declarado na função torrarPao, 
    na sua iniciação não é necessário a utilização do tipo. Ex: (let tipo)
 */
-function torrarPao(tipo) {
-    console.log('Torrando pão, ' + tipo)
+function torrarPao(tipo, formato, sabor = 'salgado') {
+    console.log('Torrando pão, ' + tipo + ', ' + formato + ' e ' + sabor)
 }
 
 /*Chamado da funcao torrarPao, atribuindo o 'árabe' a variável tipo 
  criada na declaração da função torrarPao */
-torrarPao('árabe')
+torrarPao('árabe', 'redondo')
 
 /* Obs: Toda variável declarada dentro do escopo da função não poderá ser
-      reconhecida de forma global fora da função. Esta só existirá dentro da função
-      a qual foi declarada. */
+        reconhecida de forma global fora da função. Esta só existirá dentro da função
+        a qual foi declarada. */
+
+/* Obs: Sempre que tentar definir o valor a variável parâmetro em uma função,
+onde a mesma não foi passada no seu chamado, o valor padrão ou defaul deverá 
+ser declarado dentro da criação da função e deverá ficar sempre como o
+última variavel a ser chamada. */
