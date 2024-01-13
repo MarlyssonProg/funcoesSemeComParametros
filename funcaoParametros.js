807b51de072e38f94fe65630d2aaf51521a0eff7
MarlyssonProg <designprogthe@gmail.com>
2024-01-12 23:58:21 -0300

    Atualizando os comentários, para melhor legibilidade do código

--- funcaoParametros.js
+++ funcaoParametros.js
@@ -1,13 +1,15 @@
-// Declaração de função com (parâmetro/variável) TIPO 
-
+/* Declaração de função com (parâmetro/variável) TIPO 
+   Como o parâmetro tipo, declarado na função torrarPao, 
+   na sua iniciação não é necessário a utilização do tipo. Ex: (let tipo)
+*/
 function torrarPao(tipo) {
     console.log('Torrando pão, ' + tipo)
 }
 
-// Chamado da funcao torrarPao, atribuindo o 'árabe' a variável tipo 
-// criada na declaração da função torrarPao  
+/*Chamado da funcao torrarPao, atribuindo o 'árabe' a variável tipo 
+ criada na declaração da função torrarPao */
 torrarPao('árabe')
 
-// Obs: Toda variável declarada dentro do escopo da função não poderá ser
-//      reconhecida de forma global fora da função. Esta só existirá dentro da função
-//      a qual foi declarada.
+/* Obs: Toda variável declarada dentro do escopo da função não poderá ser
+      reconhecida de forma global fora da função. Esta só existirá dentro da função
+      a qual foi declarada. */
