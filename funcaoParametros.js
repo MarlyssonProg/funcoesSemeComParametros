b19a3b55ee1137f0a8bece830c874af4dad7f1df
MarlyssonProg <designprogthe@gmail.com>
2024-01-14 14:26:57 -0300

    Utilizando a interpolação de strings de forma mais usada

--- funcaoParametros.js
+++ funcaoParametros.js
@@ -2,13 +2,13 @@
    Como o parâmetro tipo, declarado na função torrarPao, 
    na sua iniciação não é necessário a utilização do tipo. Ex: (let tipo)
 */
-function torrarPao(tipo, formato, sabor = 'salgado') {
-    console.log('Torrando pão, ' + tipo + ', ' + formato + ' e ' + sabor)
+function torrarPao(tipo, formato, sabor) {
+    console.log(`Torrando pão, ${tipo} , ${formato} e ${sabor}`)
 }
 
 /*Chamado da funcao torrarPao, atribuindo o 'árabe' a variável tipo 
  criada na declaração da função torrarPao */
-torrarPao('árabe', 'redondo')
+torrarPao('árabe', 'quadrado', 'doce')
 
 /* Obs: Toda variável declarada dentro do escopo da função não poderá ser
         reconhecida de forma global fora da função. Esta só existirá dentro da função
